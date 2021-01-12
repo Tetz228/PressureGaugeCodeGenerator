9fbc23290c9a396d7ca4b2a43018393c6f588bf5
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-12 16:11:28 +0700

    Создание окна "Поддержка"

---
+++ PressureGaugeCodeGenerator/ViewModels/HelpWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using PressureGaugeCodeGenerator.ViewModels.Base;
+using PressureGaugeCodeGenerator.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace PressureGaugeCodeGenerator.ViewModels
+{
+    class HelpWindowViewModel: ViewModel
+    {
+        #region Заголовок окна
+
+        private string _Title = "Поддержка";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseWindowCommand { get; }
+
+        private void OnCloseWindowCommandExecuted(object p)
+        {
+            
+        }
+
+        private bool CanCloseWindowCommandExecuted(object p) => true;
+
+        #endregion
+
+        #endregion
+    }
+}
