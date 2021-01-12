e37ec4ff2197f3471c63c7b7e977191e814313be
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-12 09:48:25 +0700

    Добавление заголовка окна

--- PressureGaugeCodeGenerator/ViewModels/MainWindowViewModel.cs
+++ PressureGaugeCodeGenerator/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PressureGaugeCodeGenerator.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace PressureGaugeCodeGenerator.ViewModels
 {
-    class MainWindowViewModel
+    class MainWindowViewModel: ViewModel
     {
+        #region Заголовок окна
+
+        private string _Title = "Генератор кодов манометров";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
