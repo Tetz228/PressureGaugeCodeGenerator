9913ff42df3c50aaf936a6ec764a94e6e0bf9ba8
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-12 13:40:52 +0700

    Удаление лишних библиотек

--- PressureGaugeCodeGenerator/Infrastructure/Commands/CloseApplicationCommand.cs
+++ PressureGaugeCodeGenerator/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -1,9 +1,4 @@
 ﻿using PressureGaugeCodeGenerator.Infrastructure.Commands.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace PressureGaugeCodeGenerator.Infrastructure.Commands
