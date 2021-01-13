04fec3021edb599b1a0ead31b52456b3f4720e34
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-13 10:42:19 +0700

    Добавление описания поддержки

--- PressureGaugeCodeGenerator/Views/Windows/HelpWindow.xaml.cs
+++ PressureGaugeCodeGenerator/Views/Windows/HelpWindow.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PressureGaugeCodeGenerator.Views.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для HelpWindow.xaml
-    /// </summary>
     public partial class HelpWindow : Window
     {
         public HelpWindow()
         {
             InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
