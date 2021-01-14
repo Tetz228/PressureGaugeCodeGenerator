718d2fb66b9932d5c78a4703c0816f2e955d7e4f
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-14 12:25:21 +0700

    Удаление мусора

--- PressureGaugeCodeGenerator/Infrastructure/Commands/Base/Command.cs
+++ PressureGaugeCodeGenerator/Infrastructure/Commands/Base/Command.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace PressureGaugeCodeGenerator.Infrastructure.Commands.Base
@@ -16,6 +12,6 @@ namespace PressureGaugeCodeGenerator.Infrastructure.Commands.Base
         }
 
         public abstract bool CanExecute(object parameter);
-        public abstract void Execute(object parameter); 
+        public abstract void Execute(object parameter);
     }
 }
