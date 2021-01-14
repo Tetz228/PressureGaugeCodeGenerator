9887a662f132949d2a3e9652b06dbfca5d8b04bc
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-14 12:26:24 +0700

    Добавление моделей

---
+++ PressureGaugeCodeGenerator/Models/Departments.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressureGaugeCodeGenerator.Models
+{
+    internal class Departments: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region Номер участка
+        private int _Number;
+
+        /// <summary>Номер участка</summary>
+        public int Number 
+        {
+            get=> _Number; 
+            set 
+            {
+                _Number = value;
+                OnPropertyChanged("Number");
+            } 
+        }
+        #endregion
+
+        #region Название участка
+        private string _Name;
+
+        /// <summary>Название участка</summary>
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        } 
+        #endregion
+    }
+}
---
+++ PressureGaugeCodeGenerator/Models/Manometers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressureGaugeCodeGenerator.Models
+{
+    internal class Manometers: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region Номер участка
+        private int _Number;
+
+        /// <summary>Номер участка</summary>
+        public int Number
+        {
+            get => _Number;
+            set
+            {
+                _Number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        #endregion
+    }
+}
