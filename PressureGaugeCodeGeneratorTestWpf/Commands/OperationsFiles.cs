0aa2f81cede120e349b975e17f69f1222741b8c1
Tetz <tetz2281337@gmail.com>
2021-01-16 21:31:48 +0700

    Добавлен метод генерации. Исправление коммита

--- PressureGaugeCodeGeneratorTestWpf/Commands/OperationsFiles.cs
+++ PressureGaugeCodeGeneratorTestWpf/Commands/OperationsFiles.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace PressureGaugeCodeGeneratorTestWpf.Commands
 {
@@ -16,7 +17,7 @@ namespace PressureGaugeCodeGeneratorTestWpf.Commands
         #region Проверка на то, существует ли файл
         /// <summary>Проверка на то, существует ли файл</summary>
         /// <param name="path">Строка пути до файла</param>
-        /// <returns>Возвращает true, если файла существует, иначе false</returns>
+        /// <returns>Возвращает true, если путь до файла существует, иначе false</returns>
         public static bool FileExist(string path) => File.Exists(path);
         #endregion
 
@@ -54,5 +55,86 @@ namespace PressureGaugeCodeGeneratorTestWpf.Commands
         /// <returns>Возвращает последние 2 цифры года</returns>
         public static string GetYear() => DateTime.Now.ToString("yy");
         #endregion
+
+        //public void Generate(int _begin, int _quantity, string _path, int _count)
+        //{
+        //    string path = _path;
+        //    if (_path.Length > 0)
+        //    {
+        //        if (!CheckPath(_path))
+        //        {
+        //            MessageBox.Show(
+        //                "Путь " + _path + " не найден",
+        //                "Ошибка",
+        //                MessageBoxButton.OK,
+        //                MessageBoxImage.Error);
+        //        }
+        //        else
+        //        {
+        //            if (checkBox_setYear.IsChecked == false)
+        //            {
+        //                string year = textBox_begin.Text.Substring(0, 2);
+        //                path = Directory.GetCurrentDirectory() + "\\numbers" + GetDepartment() + "_20" + year + ".txt";
+        //                textBox_path.Text = path;
+        //                //TODO: тут создаём файл с маркировкой года, если проверка года отключена
+        //            }
+        //            if (!FileExist(path))
+        //            {
+        //                MessageBoxResult result = MessageBox.Show(
+        //                    "Файл " + path + " не существует\nСоздать файл?",
+        //                    "Ошибка",
+        //                    MessageBoxButton.YesNo,
+        //                    MessageBoxImage.Error);
+
+        //                switch (result)
+        //                {
+        //                    case MessageBoxResult.Yes:
+        //                        using (FileStream fs = File.Create(path))
+        //                        {
+        //                            MessageBox.Show("Создан файл" + path, "Информация");
+        //                        }
+        //                        Generate(_begin, _quantity, path, _count);
+        //                        //     textBox_begin.Text = GetYear() + GetDepartment() + "000001";
+        //                        break;
+        //                    case MessageBoxResult.No:
+        //                        break;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                int num = Int32.Parse(textBox_begin.Text.Substring(0, 2));
+        //                if (checkBox_setYear.IsChecked == true && num != Int32.Parse(GetYear()))
+        //                {
+        //                    MessageBox.Show(
+        //                        "Вы пытаетесь сгенерировать номера для 20" + num + "-го года, " +
+        //                        "сейчас 20" + Int32.Parse(GetYear()) + "-й год. " +
+        //                        "Чтобы отключить проверку, снимите галочку \"Установить год автоматически\"",
+        //                        "Ошибка",
+        //                        MessageBoxButton.OK,
+        //                        MessageBoxImage.Error);
+        //                }
+        //                else
+        //                {
+        //                    string str = _begin.ToString();
+        //                    string department = GetDepartment();
+        //                    if (str[2] != department[0])
+        //                    {
+        //                        str = str.Remove(2, 1).Insert(2, department[0].ToString());
+        //                        _begin = Int32.Parse(str);
+        //                    }
+        //                    WriteFile(path, _begin, _quantity, _count);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show(
+        //                "Путь к файлу не должен быть пустым",
+        //                "Ошибка",
+        //                MessageBoxButton.OK,
+        //                MessageBoxImage.Error);
+        //    }
+        //}
     }
 }
