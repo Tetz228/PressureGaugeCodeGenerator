5fec19cc6ae8f8953b7dafa336bd979c3746e7aa
Tetz <tetz2281337@gmail.com>
2021-01-16 17:51:03 +0700

    Создание класс для констант

---
+++ PressureGaugeCodeGeneratorTestWpf/Data/GlobalVar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressureGaugeCodeGeneratorTestWpf.Data
+{
+    public static class GlobalVar
+    {
+        public const int DIGITS = 9;
+        public const int QR_WIDTH = 21;
+        public const int QR_HEIGHT = 21;
+    }
+}
