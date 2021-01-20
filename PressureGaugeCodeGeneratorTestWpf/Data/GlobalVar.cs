55697cd5b29b71378facced4984083c26087dde4
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-20 11:33:50 +0700

    Удаление ненужной константы. Добавление новой константы

--- PressureGaugeCodeGeneratorTestWpf/Data/GlobalVar.cs
+++ PressureGaugeCodeGeneratorTestWpf/Data/GlobalVar.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PressureGaugeCodeGeneratorTestWpf.Data
+﻿namespace PressureGaugeCodeGeneratorTestWpf.Data
 {
     public static class GlobalVar
     {
         public const int DIGITS = 9;
-        public const int QR_WIDTH = 21;
-        public const int QR_HEIGHT = 21;
+        public const string NAME_FOLDER_QR = "QR-codes\\";
     }
 }
