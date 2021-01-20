f8706dbe02e23a8fc4871592c0fd3412f4dc58b4
Кирилл Зыков <tetz2281337@gmail.com>
2021-01-20 11:35:45 +0700

    Добавлены библиотеки. Добавлено свойство, которое содержит путь до файла. Описано событие при загрузки окна

--- PressureGaugeCodeGeneratorTestWpf/Windows/ListNumbersWindow.xaml.cs
+++ PressureGaugeCodeGeneratorTestWpf/Windows/ListNumbersWindow.xaml.cs
@@ -1,17 +1,38 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 
 namespace PressureGaugeCodeGeneratorTestWpf.Windows
 {
     public partial class ListNumbersWindow : Window
     {
-        public ListNumbersWindow()
+        private readonly string Patch;
+
+        public ListNumbersWindow(string _patch)
         {
+            Patch = _patch;
             InitializeComponent();
         }
 
         private void ButtonOK_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void ListNumbersWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var streamReader = new StreamReader(Patch))
+                {
+                    string numbers = streamReader.ReadToEnd();
+                    TextBoxNumbers.Text = numbers == "" ? "Номера в файле отсутствуют!" : numbers;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка при чтении файла с номерами", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
